fa440349a3ea7408a57fb4fc722476aeaeb234c9
Nimesh Kavinda <44240093+nimeshkavinda@users.noreply.github.com>
2020-10-04 12:22:25 +0530

    Updates

---
+++ precache-manifest.9749786de82945870b9bdbd5f28d0d1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf2121296d173099d204c57a6bfc2bb",
+    "url": "/integrating-project/index.html"
+  },
+  {
+    "revision": "a512758138f8b9957980",
+    "url": "/integrating-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5cc22eb2f83443bdd7b3",
+    "url": "/integrating-project/static/js/2.9a6756dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/integrating-project/static/js/2.9a6756dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a512758138f8b9957980",
+    "url": "/integrating-project/static/js/main.a575867f.chunk.js"
+  },
+  {
+    "revision": "8c3f2b0ae97b5277531c",
+    "url": "/integrating-project/static/js/runtime-main.fc44835e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/integrating-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
